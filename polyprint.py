1809dd79dc775ae12038c45d433db041b6e5e1c2
Eric Darr <darre@mail.gvsu.edu>
2019-10-09 02:28:10 -0400

    initial commit

---
+++ polyprint.py
@@ -0,0 +1,53 @@
+from pprint import pprint
+from sympy import Matrix
+
+def genMatrix(s):
+    power = len(s)
+    m = []
+    x = 0
+    for c in s:
+        row = [ord(c)]
+        for p in range(0, power):
+            row.insert(0, x**p)
+        print(ord(c), c, row)
+        m.append(row)
+        x += 1
+    return m
+
+print("Enter word to get polynomial: ")
+words = input()
+word_len = len(words)
+m = genMatrix(words)
+
+print("\nMatrix to evaluate: \n")
+mat = Matrix(m)
+pprint(mat)
+rref = mat.rref()
+print("\nRow Reduced Matrix: \n")
+pprint(rref)
+
+print("----------------------------------------------------------------")
+print("\nEquation for \"{w}\" where the range of x integers from 0 to {l}: \n".format(w=words, l=word_len))
+print("----------------------------------------------------------------")
+i = 0
+g_term = ""
+c_term = ""
+p_term = ""
+j_term = ""
+for c in words:
+    g_term += "(" + str(rref[0][word_len+(i*(word_len+1))]) + ".0)*(x^" + str(word_len-i-1) + ")"
+    c_term += "(" + str(rref[0][word_len+(i*(word_len+1))]) + ".0)*pow(x," + str(word_len-i-1) + ")"
+    p_term += "(" + str(rref[0][word_len+(i*(word_len+1))]) + ".0)*(x**" + str(word_len-i-1) + ")"
+    j_term += "(" + str(rref[0][word_len+(i*(word_len+1))]) + ".0)*Math.pow(x," + str(word_len-i-1) + ")"
+    if(i != word_len-1):
+        g_term += " + "
+        c_term += " + "
+        p_term += " + "
+        j_term += " + "
+    i += 1
+
+print("\nGeneral:", g_term.replace("/", ".0/"))
+print("\nPython:", p_term.replace("/", ".0/"))
+print("\nC/C++:", c_term.replace("/", ".0/"))
+print("\nJava:", j_term.replace("/", ".0/"))
+
